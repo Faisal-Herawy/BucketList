e5f58ebd2b3b91ffa86717db41fd4c647cc9fee5
Faisal-Herawy <faisalherawy@icloud.com>
2024-01-24 13:50:52 +0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Faisal Herawy on 12/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Faisal Herawy on 12/07/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
